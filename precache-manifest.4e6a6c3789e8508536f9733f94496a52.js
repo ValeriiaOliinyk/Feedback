ee745fa9a60b38e7e1db0d9ba95eb6afd38a5d4d
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-06-18 17:23:23 +0300

    Updates

---
+++ precache-manifest.4e6a6c3789e8508536f9733f94496a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc4e08f40232f14e6e7f64e02f6cafb",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "ba175ebbc3f466064b82",
+    "url": "/goit-react-hw-02-feedback/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "ba175ebbc3f466064b82",
+    "url": "/goit-react-hw-02-feedback/static/js/2.71a073b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.71a073b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4044ab5749d128f787b0",
+    "url": "/goit-react-hw-02-feedback/static/js/main.46d389bc.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
